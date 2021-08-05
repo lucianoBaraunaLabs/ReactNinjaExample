2853d6bfc5a96d07eb7a8e915c31ff0a31b8d51d
Luciano Baraúna <baraunaluciano@gmail.com>
2021-08-04 22:32:33 -0300

    finalizada aula m4#a121

--- my-examples/m04/reactzzaria/sistema-pedidos/src/pages/choose-pizza-quantity/choose-pizza-quantity.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/pages/choose-pizza-quantity/choose-pizza-quantity.js
@@ -1,7 +1,40 @@
 import React from 'react'
+import styled from 'styled-components'
+import {
+  Input as MaterialInput
+} from '@material-ui/core'
+import { Content, HeaderContent, H4 } from 'ui'
 
 function ChoosePizzaQuantity () {
-  return (<p>OLá mundo</p>)
+  return (
+    <Content>
+      <HeaderContent>
+        <H4>
+          Quantas pizzas você gostaria<br /> de pedir, com esses sabores ?
+        </H4>
+      </HeaderContent>
+      <MainContent>
+        <Input defaultValue='1' autoFocus />
+      </MainContent>
+
+    </Content>
+  )
 }
+const MainContent = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: ${({ theme }) => theme.spacing(2)}px
+`
+
+const Input = styled(MaterialInput).attrs({
+  type: 'number'
+})`
+  & input {
+    font-size: 80px;
+    padding: 10px;
+    text-align: center;
+    width: 150px;
+  }
+`
 
 export default ChoosePizzaQuantity
