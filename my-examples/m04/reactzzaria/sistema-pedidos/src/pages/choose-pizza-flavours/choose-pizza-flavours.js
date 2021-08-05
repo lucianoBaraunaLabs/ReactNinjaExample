cea2dd2c5e8d81f0d1181d5646bf423294d47975
Luciano Baraúna <baraunaluciano@gmail.com>
2021-08-04 22:49:50 -0300

    finalizada aula m4#a121

--- my-examples/m04/reactzzaria/sistema-pedidos/src/pages/choose-pizza-flavours/choose-pizza-flavours.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/pages/choose-pizza-flavours/choose-pizza-flavours.js
@@ -1,31 +1,25 @@
 import React, { useState } from 'react'
 import t from 'prop-types'
-import { Redirect, Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import styled from 'styled-components'
 import {
-  Button as MaterialButton,
   Card as MaterialCard,
-  Container,
   Grid,
   Typography
 } from '@material-ui/core'
-import { CardLink, Content, Divider, HeaderContent, H3, PizzasGrid } from 'ui'
+import { CardLink, Content, Divider, Footer, HeaderContent, H3, PizzasGrid } from 'ui'
 import { singularOrPlural, toMoney } from 'utils'
-import { HOME, CHOOSE_PIZZA_QUANTITY } from 'routes'
-import { useAuth } from 'hooks'
+import { HOME } from 'routes'
 
 import pizzaFlavours from 'fake-data/pizzas-flavours'
 
 const ChoosePizzaFlavours = ({ location }) => {
   const [checkboxes, setCheckboxes] = useState({})
-  const { userInfo } = useAuth()
-  console.log('userInfo: ', userInfo)
-  console.log('logo de cara', checkboxes)
   if (!location.state) {
     return <Redirect to={HOME} />
   }
 
-  const { flavours, id, slices, name } = location.state
+  const { flavours, id } = location.state
   console.log(location.state)
 
   const handleChangeCheckBox = (pizzaId) => (e) => {
@@ -72,27 +66,7 @@ const ChoosePizzaFlavours = ({ location }) => {
           ))}
         </PizzasGrid>
       </Content>
-      <Footer>
-        <Container>
-          <Grid container>
-            <OrderContainer item>
-              <Typography>
-                <b>{userInfo.user.firstName}, seu pedido é:</b>
-              </Typography>
-              <Typography>
-                Pizza <b>{name.toUpperCase()}</b> {'- '}
-                ({slices} {singularOrPlural(slices, 'fatia', 'fatias')} {'- '}
-                {flavours} {singularOrPlural(flavours, 'sabor', 'sabores')})
-              </Typography>
-            </OrderContainer>
-            <Grid item>
-              <Button to={HOME}>Mudar tamanho</Button>
-              <Button to={CHOOSE_PIZZA_QUANTITY} color='primary'>Quantas pizzas ?</Button>
-            </Grid>
-          </Grid>
-
-        </Container>
-      </Footer>
+      <Footer />
     </>
   )
 }
@@ -122,24 +96,6 @@ const Label = styled(CardLink).attrs({
 
 const Img = styled.img`
   width: 200px;
-`
-const Footer = styled.footer`
-  box-shadow: 0 0 3px ${({ theme }) => theme.palette.grey[400]};
-  padding: ${({ theme }) => theme.spacing(3)}px;
-  width: 100%;
-`
-
-const OrderContainer = styled(Grid).attrs({
-  item: true
-})`
-  flex-grow: 1;
-`
-
-const Button = styled(MaterialButton).attrs({
-  variant: 'contained',
-  component: Link
-})`
-  margin-left: ${({ theme }) => theme.spacing(2)}px;
 `
 
 export default ChoosePizzaFlavours
