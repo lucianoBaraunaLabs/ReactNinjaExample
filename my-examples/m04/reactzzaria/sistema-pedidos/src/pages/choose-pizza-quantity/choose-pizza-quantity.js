3a9e3403e3dd13eff2597eb2940287679b0062cc
Luciano Baraúna <baraunaluciano@gmail.com>
2021-08-06 20:43:33 -0300

    finalizada aula m4#a124

--- my-examples/m04/reactzzaria/sistema-pedidos/src/pages/choose-pizza-quantity/choose-pizza-quantity.js
+++ my-examples/m04/reactzzaria/sistema-pedidos/src/pages/choose-pizza-quantity/choose-pizza-quantity.js
@@ -3,21 +3,36 @@ import styled from 'styled-components'
 import {
   Input as MaterialInput
 } from '@material-ui/core'
-import { Content, HeaderContent, H4 } from 'ui'
+import { Content, HeaderContent, H4, Footer } from 'ui'
+import { CHOOSE_PIZZA_FLAVOURS } from 'routes'
 
 function ChoosePizzaQuantity () {
   return (
-    <Content>
-      <HeaderContent>
-        <H4>
-          Quantas pizzas você gostaria<br /> de pedir, com esses sabores ?
-        </H4>
-      </HeaderContent>
-      <MainContent>
-        <Input defaultValue='1' autoFocus />
-      </MainContent>
+    <>
+      <Content>
+        <HeaderContent>
+          <H4>
+            Quantas pizzas você gostaria<br /> de pedir, com esses sabores ?
+          </H4>
+        </HeaderContent>
+        <MainContent>
+          <Input defaultValue='1' autoFocus />
+        </MainContent>
+      </Content>
+      <Footer buttons={[
+        {
+          to: CHOOSE_PIZZA_FLAVOURS,
+          children: 'Mudar sabores'
+        },
+        {
+          to: '/',
+          children: 'Finalizar compra',
+          color: 'primary'
+        }
+      ]}
+      />
 
-    </Content>
+    </>
   )
 }
 const MainContent = styled.div`
