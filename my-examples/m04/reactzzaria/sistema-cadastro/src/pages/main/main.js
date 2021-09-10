99b8e27328c84d3bb158befaa04e22c6d2bfdd4e
Luciano Baraúna <baraunaluciano@gmail.com>
2021-09-10 08:29:27 -0300

    finalizada aula m4#a176

--- my-examples/m04/reactzzaria/sistema-cadastro/src/pages/main/main.js
+++ my-examples/m04/reactzzaria/sistema-cadastro/src/pages/main/main.js
@@ -2,12 +2,32 @@ import React, { Suspense } from 'react'
 import styled from 'styled-components'
 import { Switch, Route } from 'react-router-dom'
 
-import { Drawer as MaterialDrawer } from '@material-ui/core'
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Drawer as MaterialDrawer,
+  Typography
+} from '@material-ui/core'
 
 const Main = () => (
   <>
     <Drawer variant='permanent'>
-      lolo
+      <DrawerContent>
+        <Typography variant='h4'>React-zzaria</Typography>
+        <Typography>(sistema de cadastro)</Typography>
+      </DrawerContent>
+
+      <Divider />
+
+      <List>
+        {menuItems.map(item => (
+          <ListItem key={item.label} button>
+            <ListItemText>{item.label}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
     </Drawer>
 
     <Content>
@@ -22,12 +42,30 @@ const Main = () => (
   </>
 )
 
+const menuItems = [
+  {
+    label: 'Pedidos'
+  },
+  {
+    label: 'Tamanhos de pizzas'
+  },
+  {
+    label: 'Sabores de pizzas'
+  }
+]
+
 const Drawer = styled(MaterialDrawer)`
   && {
     .MuiPaper-root {
       width: ${({ theme }) => theme.extend.drawerWidth}px;
     }
   }
+`
+const DrawerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: ${({ theme }) => theme.spacing(1)}px;
+  text-align: center
 `
 
 const Content = styled.main`
