271349482ca7cabd0a15b9b2e62c338ad3ebf89f
Luciano Baraúna <baraunaluciano@gmail.com>
2021-10-07 11:09:07 -0300

    finalizada aula m4#a203

---
+++ my-examples/m04/reactzzaria/sistema-cadastro/src/pages/pizzas-flavours/table.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link, useRouteMatch } from 'react-router-dom'
+import {
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableRow
+} from '@material-ui/core'
+import { Add, Delete, Edit } from '@material-ui/icons'
+import {
+  TableButton,
+  TableContainer,
+  TableTitleContainer,
+  TableTitle,
+  THead,
+  Th
+} from 'ui'
+
+import { PIZZAS_FLAVOURS, NEW, EDIT } from 'routes'
+
+function TablePizzasFlavours () {
+  const newFlavourPath = useRouteMatch(`${PIZZAS_FLAVOURS}${NEW}`)
+
+  return (
+    <TableContainer>
+      <TableTitleContainer>
+        <Grid item>
+          <TableTitle>
+            Saores cadastrados
+          </TableTitle>
+        </Grid>
+
+        <Grid item>
+          <TableButton
+            color='primary'
+            startIcon={<Add />}
+            component={Link}
+            to={`${PIZZAS_FLAVOURS}${NEW}`}
+            disabled={!!newFlavourPath}
+          >
+            Adicionar novo sabor
+          </TableButton>
+        </Grid>
+      </TableTitleContainer>
+
+    </TableContainer>
+  )
+}
+
+export default TablePizzasFlavours
